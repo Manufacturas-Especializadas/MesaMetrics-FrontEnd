8dbb1bdb9be3574417bbc3534e54178329305988
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-12-03 09:19:12 -0600

    Apis para registrar y obtencion de datos

---
+++ src/config/api.ts
@@ -0,0 +1,18 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+if (!API_BASE_URL) {
+    throw new Error("API base URL is not defined in environment variables");
+}
+
+export const API_CONFIG = {
+    baseUrl: API_BASE_URL,
+    endpoints: {
+        realTime: {
+            create: "/api/RealTime/RegisterRealTime"
+        },
+        generalLists: {
+            tagsList: "/api/GeneralList/GetTags",
+            shiftsList: "/api/GeneralList/GetShifts"
+        }
+    }
+};
