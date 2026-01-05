4eeb866c03967cb0e49f11f6548c29051d357ae1
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-05 10:11:23 -0600

    Codigo formateado

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -6,29 +6,28 @@ import { DataAnalyticsIndex } from "../pages/DataAnalytics/DataAnalyticsIndex";
 import { ReportsIndex } from "../pages/Reports/ReportsIndex";
 import { ExportDataIndex } from "../pages/ExportData/ExportDataIndex";
 import { RealTimeForm } from "../components/Forms/RealTimeForm/RealTimeForm";
-import { HistoricalShifts } from '@/pages/RealTime/HistoricalShifts';
+import { HistoricalShifts } from "@/pages/RealTime/HistoricalShifts";
 import { Login } from "@/pages/Auth/Login";
 import { Register } from "@/pages/Auth/Register";
 
 export const MyRoutes = () => {
-    return (
-        <Routes>
+  return (
+    <Routes>
+      {/* Auth */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
-            {/* Auth */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+      {/* Real Time */}
+      <Route path="/tiempo-real" element={<RealTimeIndex />} />
+      <Route path="/nueva-maquina" element={<RealTimeForm />} />
+      <Route path="/editar-maquina/:id" element={<RealTimeForm />} />
+      <Route path="/turnos-historicos" element={<HistoricalShifts />} />
 
-            {/* Real Time */}
-            <Route path="/tiempo-real" element={<RealTimeIndex />} />
-            <Route path="/nueva-maquina" element={<RealTimeForm />} />
-            <Route path="/editar-maquina/:id" element={<RealTimeForm />} />
-            <Route path="/turnos-historicos" element={<HistoricalShifts />} />
-
-            <Route path="/resumen-general" element={<GeneralSummaryIndex />} />
-            <Route path="/paradas" element={<StopsIndex />} />
-            <Route path="/analitica-de-datos" element={<DataAnalyticsIndex />} />
-            <Route path="/reportes" element={<ReportsIndex />} />
-            <Route path="/exportar-datos" element={<ExportDataIndex />} />
-        </Routes>
-    );
+      <Route path="/resumen-general" element={<GeneralSummaryIndex />} />
+      <Route path="/paradas" element={<StopsIndex />} />
+      <Route path="/analitica-de-datos" element={<DataAnalyticsIndex />} />
+      <Route path="/reportes" element={<ReportsIndex />} />
+      <Route path="/exportar-datos" element={<ExportDataIndex />} />
+    </Routes>
+  );
 };
