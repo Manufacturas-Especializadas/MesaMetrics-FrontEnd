2928ac7864aa63d6ade1e9c04ddb52a54d75fcd0
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-05 09:51:36 -0600

    Se adapto el componente para obtener el usuario

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,71 @@
 import { useState } from "react";
 import { Navbar } from "./components/layout/Navbar/Navbar";
 import { Sidebar } from "./components/layout/Sidebar/Sidebar";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Navigate, useLocation } from "react-router-dom";
 import { MyRoutes } from "./routes/Routes";
+import { AuthProvider, useAuth } from "./hooks/useAuth";
 
-export const App = () => {
+const AppLayout = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  return (
-    <>
-      <BrowserRouter>
-        <div className="bg-primary-50 min-h-screen">
-          <Navbar
-            toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)}
-          />
+  const location = useLocation();
 
-          <Sidebar isSidebarOpen={isSidebarOpen} />
-          <main
-            className={`
-            pt-20 
-            transition-all duration-300 ease-in-out
-            ${isSidebarOpen ? 'pl-64' : 'pl-20'}
-          `}
-          >
-            <div className="p-4 lg:p-8">
-              <MyRoutes />
-            </div>
-          </main>
+  const { isAuthenticated, loading } = useAuth();
+
+  const publicRoutes = ["/login", "/register", "/"];
+  const currentPath = location.pathname.toLowerCase();
+
+  const isPublicPage = publicRoutes.includes(currentPath);
+
+  if (loading) {
+    return (
+      <div className="h-screen w-full flex items-center justify-center bg-gray-50">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-200"></div>
+      </div>
+    );
+  }
+
+  if (!isAuthenticated && !isPublicPage) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (isAuthenticated && isPublicPage) {
+    return <Navigate to="/resumen-general" replace />;
+  }
+
+  if (isPublicPage) {
+    return (
+      <main className="w-full min-h-screen bg-gray-50">
+        <MyRoutes />
+      </main>
+    );
+  }
+
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <Navbar toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
+
+      <Sidebar isSidebarOpen={isSidebarOpen} />
+
+      <main
+        className={`
+                    pt-24 px-4 pb-8 lg:px-8
+                    transition-all duration-300 ease-in-out
+                    ${isSidebarOpen ? "lg:pl-72" : "lg:pl-28"} 
+                `}
+      >
+        <div className="max-w-7xl mx-auto">
+          <MyRoutes />
         </div>
+      </main>
+    </div>
+  );
+};
+export const App = () => {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <AppLayout />
       </BrowserRouter>
-    </>
+    </AuthProvider>
   );
 };
